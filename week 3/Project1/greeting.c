8c1e21462b4a84b59005c01c61b57f7289d34dfb
CJM <20251806@edu.hanbat.ac.kr>
2025-09-16 17:20:37 +0900

    finish

---
+++ week 3/Project1/greeting.c
@@ -0,0 +1,9 @@
+//greeting.c
+#include<stdio.h>
+
+int main(void) {
+	printf("\tHello Class\n");
+	printf("I said, \"Hello!\"\n"); //\ = 기능무시
+	printf("This is a backslash: \\");  //\\ = 기능무시+출력
+	return 0;
+}
---
+++ week 3/Project1/hw.c
@@ -0,0 +1,13 @@
+//hw.c
+#include<stdio.h>
+#include<math.h>
+#include "calc.h"
+
+int main(void) {
+	int p;
+	p = 1000000;
+	printf("원금 %d\\ 있습니다.", p);
+
+	double A;
+	A = p(1 + r / n) ^ (n * t);
+}
